22d78a19235c761a32767e2865a2e33dbf3ff4fe
Style71 <qiy17@tsinghua.org.cn>
2021-04-24 01:14:31 +0800

    Add UDP.

---
+++ CloudServer/UDPServer.py
@@ -0,0 +1,31 @@
+from socket import *
+from time import ctime
+from os import system
+
+#HOST = ''
+#HOST = '43.129.87.211'
+HOST = '10.0.0.11'
+#HOST = '0.0.0.0'
+PORT = 4464
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+udpServSock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+udpServSock.bind(ADDR)
+udpServSock.listen(5)
+
+print('Listening at port '+str(PORT)+'...')
+tcpClientSock, addr = udpServSock.accept()
+print('Connect from: %s/%u' % (addr[0], addr[1]))
+system('netstat -nap | grep %s:%u' % (addr[0], addr[1]))
+tcpClientSock.send(bytes('[%s]Connect to server %s/%u (inner IP/port)' %
+                   (ctime(), HOST, PORT), 'utf-8'))
+
+while True:
+    data = str(tcpClientSock.recv(BUFSIZ), encoding='utf-8')
+    if not data:
+        break
+    tcpClientSock.send(bytes('[%s]%s' % (ctime(), data), 'utf-8'))
+
+tcpClientSock.close()
+udpServSock.close()
